c9f04d5bd28d1966c2fae0c43031ba15ce5a0e36
rohitkumarmishracse30-ai <rohit.kumarmishra.cse30@heritageit.edu.in>
2026-09-11 00:36:46 +0530

    Add project files

--- compliance-scanner/backend/test_ocr.py
+++ compliance-scanner/backend/test_ocr.py
@@ -1,10 +1,10 @@
 from paddleocr import PaddleOCR
-
 ocr = PaddleOCR(
     lang="en",
     use_doc_orientation_classify=False,
     use_doc_unwarping=False,
-    use_textline_orientation=False
+    use_textline_orientation=False,
+    enable_mkldnn=False
 )
 
 image_path = input("Enter image path: ")
